267b9f69b0197be1b1de9af39fdacd12ef94a4c1
Rahul Chauhan <rahul.s.chauhan@ge.com>
2017-12-21 22:45:03 +0530

    service url to get from property.

--- src/main/java/com/rah/feign/config/FeignClientRepositoryConfiguration.java
+++ src/main/java/com/rah/feign/config/FeignClientRepositoryConfiguration.java
@@ -5,6 +5,7 @@ import feign.Feign;
 import feign.Logger;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,9 @@ import javax.inject.Inject;
 
 @Configuration
 public class FeignClientRepositoryConfiguration {
+
+    @Value("${feign_client_service_base_url:https//:abc.com}")
+    private String serviceUrl;
 
     @Inject
     Encoder encoder;
@@ -26,6 +30,6 @@ public class FeignClientRepositoryConfiguration {
                 .encoder(encoder)
                 .decoder(decoder)
                 .logLevel(Logger.Level.BASIC)
-                .target(IFeignClientRepository.class, "https://httpbin.org");
+                .target(IFeignClientRepository.class, serviceUrl);
     }
 }
